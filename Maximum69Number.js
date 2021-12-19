b9ece93218434f6672947bb1969f654375e694c4
Dan Fellner <dfellner92@gmail.com>
2021-12-18 19:06:13 -0500

    better max 69 sol

--- Maximum69Number.js
+++ Maximum69Number.js
@@ -12,3 +12,16 @@ var maximum69Number = function (num) {
   }
   return splitNum.map((num) => parseInt(num)).join("");
 };
+
+// Better solution, better space compl
+
+var maximum69Number2 = function (num) {
+  let splitNum = String(num).split("");
+  for (let i = 0; i < splitNum.length; i++) {
+    if (splitNum[i] === "6") {
+      splitNum[i] = "9";
+      break;
+    }
+  }
+  return Number(splitNum.join(""));
+};
